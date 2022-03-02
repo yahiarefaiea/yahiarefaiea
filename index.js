b98250be467641c712c4055029a40c2cae66afdf
Yahia Refaiea <yahiarefaiea@gmail.com>
2022-03-03 01:51:22 +0200

    read yaml file

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const {readFileSync} = require('fs')
+const {join} = require('path')
+const yaml = require('js-yaml')
+
+let profile = yaml.load(readFileSync(join(__dirname, './profile.yaml')))
+
 module.exports = {
   profile,
   // channels,
