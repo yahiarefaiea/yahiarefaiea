b2db34070922705402f483592eb8439590bb1f96
Yahia Refaiea <yahiarefaiea@gmail.com>
2022-10-25 07:17:45 +0800

    remove else {}

--- scripts/format-date.js
+++ scripts/format-date.js
@@ -22,17 +22,15 @@ module.exports = function(list) {
         let yymmd1 = moment(item.date[0], FORMAT).format('MMMM D, YYYY')
         let yymmd2 = moment(item.date[1], FORMAT).format('MMMM D, YYYY')
         item.formated_date = `${yymmd1} → ${yymmd2}`
-      } else {
-        if(date1[MONTH] !== date2[MONTH]) {
-          let mmd1 = moment(item.date[0], FORMAT).format('MMMM D')
-          let mmd2 = moment(item.date[1], FORMAT).format('MMMM D')
-          item.formated_date = `${mmd1} → ${mmd2}, ${date1[YEAR]}`
-        } else {
-          let d1 = moment(item.date[0], FORMAT).format('D')
-          let d2 = moment(item.date[1], FORMAT).format('D')
-          let month = moment(date2[MONTH], 'MM').format('MMMM')
-          item.formated_date = `${month} ${d1} → ${d2}, ${date1[YEAR]}`
-        }
+      } else if(date1[YEAR] === date2[YEAR] && date1[MONTH] !== date2[MONTH]) {
+        let mmd1 = moment(item.date[0], FORMAT).format('MMMM D')
+        let mmd2 = moment(item.date[1], FORMAT).format('MMMM D')
+        item.formated_date = `${mmd1} → ${mmd2}, ${date1[YEAR]}`
+      } else if(date1[YEAR] === date2[YEAR] && date1[MONTH] === date2[MONTH] && date1[DAY] !== date2[DAY]) {
+        let d1 = moment(item.date[0], FORMAT).format('D')
+        let d2 = moment(item.date[1], FORMAT).format('D')
+        let mm = moment(date1[MONTH], 'MM').format('MMMM')
+        item.formated_date = `${mm} ${d1} → ${d2}, ${date1[YEAR]}`
       }
     }
   }
