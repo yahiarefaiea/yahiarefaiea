a6af21e22de1e3245fc6bebd68ae4e00ce0e1776
Yahia Refaiea <yahiarefaiea@gmail.com>
2022-10-25 07:53:10 +0800

    format dates when preparing json

--- scripts/prepare-json.js
+++ scripts/prepare-json.js
@@ -1,9 +1,17 @@
 const {join} = require('path')
 const {readdir, readFileSync} = require('fs')
 const {outputFile} = require('fs-extra')
+const {includes} = require('lodash')
 const yaml = require('js-yaml')
+const formatDate = require('./format-date')
 const contentDirectory = join(__dirname, '../content')
 const outputDirectory = join(__dirname, '../json')
+const toFormatDate = [
+  'experience',
+  'press-and-recognition',
+  'talks-and-workshops',
+  'volunteering-activities'
+]
 
 readdir(contentDirectory, function(err, files) {
   if(err) throw err
@@ -12,6 +20,8 @@ readdir(contentDirectory, function(err, files) {
     let outputPath = join(outputDirectory, file.replace('.yaml', '.json'))
     let buffer = readFileSync(filePath)
     buffer = yaml.load(buffer)
+    if(includes(toFormatDate, file.replace('.yaml', '')))
+      buffer.items = formatDate(buffer.items)
     buffer = JSON.stringify(buffer)
 
     outputFile(outputPath, buffer, function(err) {
