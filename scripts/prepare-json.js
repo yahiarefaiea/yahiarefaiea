435173ec9b4f034858611e208f357ea9a09b536a
Yahia Refaiea <yahiarefaiea@gmail.com>
2022-10-25 03:45:21 +0800

    add scripts/prepare-json.js

---
+++ scripts/prepare-json.js
@@ -0,0 +1,21 @@
+const {readdir, readFileSync} = require('fs')
+const {outputFile} = require('fs-extra')
+const {join} = require('path')
+const yaml = require('js-yaml')
+const contentDirectory = join(__dirname, '../content')
+const outputDirectory = join(__dirname, '../json')
+
+readdir(contentDirectory, function(err, files) {
+  if(err) throw err
+  files.forEach((file) => {
+    let filePath = join(contentDirectory, file)
+    let outputPath = join(outputDirectory, file.replace('.yaml', '.json'))
+    let buffer = readFileSync(filePath)
+    buffer = yaml.load(buffer)
+    buffer = JSON.stringify(buffer)
+
+    outputFile(outputPath, buffer, function(err) {
+      if(err) throw err
+    })
+  })
+})
