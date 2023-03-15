20fdee0e65c8cfa925bfe57c3eb72ffdacae245a
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-03-15 19:28:27 +0800

    quick fix for prepare-json

--- scripts/prepare-json.js
+++ scripts/prepare-json.js
@@ -5,7 +5,7 @@ const {includes} = require('lodash')
 const yaml = require('js-yaml')
 const formatDate = require('./format-date')
 const contentDirectory = join(__dirname, '../lists') // ../content
-const outputDirectory = join(__dirname, '../json')
+const outputDirectory = join(__dirname, '../json/lists') // ../json
 const toFormatDate = [
   'experience',
   'press-and-recognition',
@@ -13,16 +13,20 @@ const toFormatDate = [
   'volunteering-activities'
 ]
 
-readdir(contentDirectory, function(err, files) {
-  if(err) throw err
+// const slugify = require('slugify') // "slugify": "^1.6.5"
+//
+// for(let cc in cases)
+//   cases[cc].shortcut = slugify(cases[cc].title, {lower: true}) // or maybe just slug
+
+
+function test() {
+  const files = ['bucket-list.yaml']
 
   files.forEach((file) => {
     let filePath = join(contentDirectory, file)
     let outputPath = join(outputDirectory, file.replace('.yaml', '.json'))
     let buffer = readFileSync(filePath)
     buffer = yaml.load(buffer)
-    if(includes(toFormatDate, file.replace('.yaml', '')))
-      buffer.items = formatDate(buffer.items)
     buffer = JSON.stringify(buffer)
 
     outputFile(outputPath, buffer, function(err) {
@@ -37,4 +41,34 @@ readdir(contentDirectory, function(err, files) {
   outputFile(indexPath, indexBuffer, function(err) {
     if(err) throw err
   })
-})
+}
+test()
+
+
+// --------------------------
+
+// readdir(contentDirectory, function(err, files) {
+//   if(err) throw err
+//
+//   files.forEach((file) => {
+//     let filePath = join(contentDirectory, file)
+//     let outputPath = join(outputDirectory, file.replace('.yaml', '.json'))
+//     let buffer = readFileSync(filePath)
+//     buffer = yaml.load(buffer)
+//     if(includes(toFormatDate, file.replace('.yaml', '')))
+//       buffer.items = formatDate(buffer.items)
+//     buffer = JSON.stringify(buffer)
+//
+//     outputFile(outputPath, buffer, function(err) {
+//       if(err) throw err
+//     })
+//   })
+//
+//   let indexPath = join(outputDirectory, 'index.json')
+//   for(let i in files)
+//     files[i] = files[i].replace('.yaml', '.json')
+//   let indexBuffer = JSON.stringify(files)
+//   outputFile(indexPath, indexBuffer, function(err) {
+//     if(err) throw err
+//   })
+// })
